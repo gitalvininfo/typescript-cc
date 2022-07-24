3200d3fa00b202802c844200faa3461f7e64ef29
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-24 12:35:53 +0800

    function types

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,22 @@
-console.log("test")
+let greet: Function;
+
+greet = () => {
+    console.log("hello")
+}
+
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+    console.log(a + b);
+    console.log(c)
+}
+
+
+add(1, 2, '20');
+
+const minus = (a: number, b: number): number => {
+    return a + b;
+}
+
+
+let result = minus(10, 7);
+console.log(result)
