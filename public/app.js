9b8bf562667ea8695b7d93fa0b68678dde08ae3e
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-24 13:20:19 +0800

    classes

--- public/app.js
+++ public/app.js
@@ -1,6 +1,23 @@
 "use strict";
 // const anchor = document.querySelector("a")!;
 // console.log(anchor.href)
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3 " + this.amount + " for " + this.details + ".";
+    };
+    return Invoice;
+}());
+var inv1 = new Invoice('mario', 'work on the mario website', 100);
+var inv2 = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(inv1);
+invoices.push(inv2);
+console.log(invoices);
 var form = document.querySelector(".new-item-form");
 // inputs
 var type = document.querySelector("#type");
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,42 @@
 // const anchor = document.querySelector("a")!;
 // console.log(anchor.href)
 
+
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+
+    constructor(client: string, details: string, amount: number) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+
+
+    format(): string {
+        return `${this.client} owes £ ${this.amount} for ${this.details}.`;
+    }
+
+}
+
+
+const inv1 = new Invoice('mario', 'work on the mario website', 100);
+const inv2 = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = []
+
+invoices.push(inv1);
+invoices.push(inv2);
+
+console.log(invoices)
+
+
+
+
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // inputs
 
